7494966d9854a74ef35127911b5594209deb20c3
pixrael <pixrael777@gmail.com>
2021-02-21 23:20:16 -0300

    t2-styled product list for more than one product

--- ml-app/src/components/product/product-item/product-item.js
+++ ml-app/src/components/product/product-item/product-item.js
@@ -2,35 +2,84 @@ import './product-item.scss';
 import shippingIcon from '../../../assets/ic_shipping.png';
 
 function ProductItem(props) {
+
     return (
-        <div className="row-product">
-            <img className="img-product" src={props.imgUrl} />
-            <div className="product-info">
-                <div className="left-price-title" >
-                    <div className="price-container">
-                        <div className="price" >
-                            $ 1.980
+        <div class="row">
+            <div className="product-container">
+                <div className="row-product">
+                    <img className="img-product" src={props.imgUrl} />
+                    <div className="product-info">
+                        <div className="left-price-title" >
+                            <div className="price-container">
+                                <div className="price" >
+                                    $ 1.980
                         </div>
-                        <div className="shipping-container" >
-                            <img src={shippingIcon} />
+                                <div className="shipping-container" >
+                                    <img src={shippingIcon} />
+                                </div>
+                            </div>
+                            <div className="title" >
+                                Gavilan como nuevo
+                    </div>
+                            <div className="status" >
+                                Completo Único!
+                    </div>
                         </div>
-
-                    </div>
-
-                    <div className="title" >
-                        Gavilan como nuevo
-                    </div>
-                    <div className="status" >
-                        Completo Único!
-                    </div>
+                        <div className="right-location" >
+                            Buenos Aires
                 </div>
-                <div className="right-location" >
-                    Buenos Aires
+
+                    </div>
                 </div>
 
             </div>
+
+
+            <div className="divider">
+                <div className="inner"></div>
+            </div>
+
+
         </div>
     );
+
+
+    /* return (
+
+        <div className="product-container">
+            <div className="row-product">
+                <img className="img-product" src={props.imgUrl} />
+                <div className="product-info">
+                    <div className="left-price-title" >
+                        <div className="price-container">
+                            <div className="price" >
+                                $ 1.980
+                        </div>
+                            <div className="shipping-container" >
+                                <img src={shippingIcon} />
+                            </div>
+                        </div>
+                        <div className="title" >
+                            Gavilan como nuevo
+                    </div>
+                        <div className="status" >
+                            Completo Único!
+                    </div>
+                    </div>
+                    <div className="right-location" >
+                        Buenos Aires
+                </div>
+
+                </div>
+            </div>
+            <div className="divider">
+                <div className="inner"></div>
+            </div>
+        </div>
+
+
+
+    ); */
 }
 
 export default ProductItem;
--- ml-app/src/components/product/product-list/product-list.js
+++ ml-app/src/components/product/product-list/product-list.js
@@ -1,11 +1,22 @@
 import ProductItem from '../product-item/product-item';
+import './product-list.scss';
 
 function ProductList() {
 
     const imgUrl = 'https://vignette2.wikia.nocookie.net/reinoanimalia/images/d/dd/Gavilan_colirrojo_7.png/revision/latest?cb=20151214221805&path-prefix=es';
 
+    const ret = [1, 2].map((val, index) => {
+        return (
+            <ProductItem key={index.toString()} className="product-item" imgUrl={imgUrl} />
+        );
+    });
+
+
     return (
-        <ProductItem imgUrl={imgUrl} />
+        <div className="item-wrapper" >
+            {ret}
+        </div>
+
     );
 
 }
